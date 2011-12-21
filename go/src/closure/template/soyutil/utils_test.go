958742efa8462129f3f9431ea1fc3093a48ad4be
Aalok Shah <aalok@pomack.com>
2011-12-20 19:35:34 -0500

    Move escaping functions to sanitizers.go

--- go/src/closure/template/soyutil/utils_test.go
+++ go/src/closure/template/soyutil/utils_test.go
@@ -5,17 +5,6 @@ import (
   "testing"
 )
 
-func TestEscapeHtml(t *testing.T) {
-  unescapedHtml := []string{"", "eat & be merry", "1 < 2", "1 < 2 < 3 > 0", "gutenberg"}
-  escapedHtml := []string{"", "eat &amp; be merry", "1 &lt; 2", "1 &lt; 2 &lt; 3 &gt; 0", "gutenberg"}
-  for i := 0; i < len(unescapedHtml); i++ {
-    s := EscapeHtml(unescapedHtml[i])
-    if s != escapedHtml[i] {
-      t.Error("EscapeHtml(\"", unescapedHtml[i], "\") -> \"", s, "\" expected: \"", escapedHtml[i], "\"")
-    }
-  }
-}
-
 
 func TestGetData(t *testing.T) {
   s := NewSoyMapDataFromArgs("name", "Albert Einstein", "occupation", NewStringData("Patent Clerk"), "birth_year", 1879)
